e2079af08b0817c8d617f5179c6c2728c5e8aec6
Nguyễn Thị Các Các <146409028+Nguyen-T-C-Cac@users.noreply.github.com>
2025-03-17 08:54:35 +0700

    50 sp & data cac bang khac

---
+++ Insert database clock.sql
@@ -0,0 +1,293 @@
+
+INSERT INTO Categories (categoryName) VALUES
+('Luxury'),
+('Classic'),
+('Smartwatch'),
+('Sport');
+
+INSERT INTO Stocks (stockName, quatityStock, minQuatity, updateStockAt, productId) VALUES
+('Kho A', 50, 10, NOW(), 1),
+('Kho B', 30, 5, NOW(), 2),
+('Kho C', 20, 2, NOW(), 3),
+('Kho D', 50, 15, NOW(), 4),
+('Kho A', 50, 10, NOW(), 5),
+('Kho B', 30, 5, NOW(), 6),
+('Kho C', 20, 2, NOW(), 7),
+('Kho D', 50, 15, NOW(), 8),
+('Kho A', 40, 10, NOW(), 9),
+('Kho B', 25, 5, NOW(), 10),
+('Kho C', 15, 2, NOW(), 11),
+('Kho D', 60, 20, NOW(), 12),
+('Kho A', 55, 10, NOW(), 13),
+('Kho B', 35, 5, NOW(), 14),
+('Kho C', 25, 2, NOW(), 15),
+('Kho D', 45, 15, NOW(), 16),
+('Kho A', 50, 10, NOW(), 17),
+('Kho B', 30, 5, NOW(), 18),
+('Kho C', 20, 2, NOW(), 19),
+('Kho D', 50, 15, NOW(), 20),
+('Kho A', 50, 10, NOW(), 21),
+('Kho B', 30, 5, NOW(), 22),
+('Kho C', 20, 2, NOW(), 23),
+('Kho D', 50, 15, NOW(), 24),
+('Kho A', 50, 10, NOW(), 25),
+('Kho B', 30, 5, NOW(), 26),
+('Kho C', 20, 2, NOW(), 27),
+('Kho D', 50, 15, NOW(), 28),
+('Kho A', 50, 10, NOW(), 29),
+('Kho B', 30, 5, NOW(), 30),
+('Kho C', 20, 2, NOW(), 31),
+('Kho D', 50, 15, NOW(), 32),
+('Kho A', 50, 10, NOW(), 33),
+('Kho B', 30, 5, NOW(), 34),
+('Kho C', 20, 2, NOW(), 35),
+('Kho D', 50, 15, NOW(), 36),
+('Kho A', 50, 10, NOW(), 37),
+('Kho B', 30, 5, NOW(), 38),
+('Kho C', 20, 2, NOW(), 39),
+('Kho D', 50, 15, NOW(), 40),
+('Kho A', 50, 10, NOW(), 41),
+('Kho B', 30, 5, NOW(), 42),
+('Kho C', 20, 2, NOW(), 43),
+('Kho D', 50, 15, NOW(), 44),
+('Kho A', 50, 10, NOW(), 45),
+('Kho B', 30, 5, NOW(), 46),
+('Kho C', 20, 2, NOW(), 47),
+('Kho D', 50, 15, NOW(), 48),
+('Kho A', 50, 10, NOW(), 49),
+('Kho B', 30, 5, NOW(), 50),
+('Kho C', 20, 2, NOW(), 51),-- them 50 sp
+('Kho D', 50, 15, NOW(), 52),
+('Kho A', 50, 10, NOW(), 53),
+('Kho B', 30, 5, NOW(), 54),
+('Kho C', 20, 2, NOW(), 55),
+('Kho D', 50, 15, NOW(), 56),
+('Kho A', 50, 10, NOW(), 57),
+('Kho B', 30, 5, NOW(), 58),
+('Kho C', 20, 2, NOW(), 59),
+('Kho D', 50, 15, NOW(), 60),
+('Kho A', 50, 10, NOW(), 61),
+('Kho B', 30, 5, NOW(), 62),
+('Kho B', 30, 5, NOW(), 62),
+('Kho B', 30, 5, NOW(), 63),
+('Kho B', 30, 5, NOW(), 64),
+('Kho B', 30, 5, NOW(), 65),
+('Kho B', 30, 5, NOW(), 66),
+('Kho B', 30, 5, NOW(), 67),
+('Kho B', 30, 5, NOW(), 68),
+('Kho B', 30, 5, NOW(), 69),
+('Kho B', 30, 5, NOW(), 70),
+('Kho B', 30, 5, NOW(), 71),
+('Kho B', 30, 5, NOW(), 72),
+('Kho B', 30, 5, NOW(), 73),
+('Kho B', 30, 5, NOW(), 74),
+('Kho B', 30, 5, NOW(), 75),
+('Kho B', 30, 5, NOW(), 76),
+('Kho B', 30, 5, NOW(), 77),
+('Kho B', 30, 5, NOW(), 78),
+('Kho B', 30, 5, NOW(), 79),
+('Kho B', 30, 5, NOW(), 80),
+('Kho B', 30, 5, NOW(), 81),
+('Kho B', 30, 5, NOW(), 82),
+('Kho B', 30, 5, NOW(), 83),
+('Kho B', 30, 5, NOW(), 84),
+('Kho B', 30, 5, NOW(), 85),
+('Kho B', 30, 5, NOW(), 86),
+('Kho B', 30, 5, NOW(), 87),
+('Kho B', 30, 5, NOW(), 88),
+('Kho B', 30, 5, NOW(), 89),
+('Kho B', 30, 5, NOW(), 90),
+('Kho C', 20, 2, NOW(), 91),
+('Kho C', 20, 2, NOW(), 92),
+('Kho C', 20, 2, NOW(), 93),
+('Kho C', 20, 2, NOW(), 94),
+('Kho C', 20, 2, NOW(), 95),
+('Kho C', 20, 2, NOW(), 96),
+('Kho C', 20, 2, NOW(), 97),
+('Kho C', 20, 2, NOW(), 98),
+('Kho C', 20, 2, NOW(), 99),
+('Kho C', 20, 2, NOW(), 100),
+('Kho C', 20, 2, NOW(), 101),
+('Kho C', 20, 2, NOW(), 102),
+('Kho C', 20, 2, NOW(), 103),
+('Kho C', 20, 2, NOW(), 104),
+('Kho C', 20, 2, NOW(), 105),
+('Kho C', 20, 2, NOW(), 106),
+('Kho C', 20, 2, NOW(), 107),
+('Kho C', 20, 2, NOW(), 108),
+('Kho C', 20, 2, NOW(), 109),
+('Kho C', 20, 2, NOW(), 110),
+('Kho C', 20, 2, NOW(), 111),
+('Kho C', 20, 2, NOW(), 112),
+('Kho C', 20, 2, NOW(), 113),
+('Kho C', 20, 2, NOW(), 114),
+('Kho C', 20, 2, NOW(), 115),
+('Kho C', 20, 2, NOW(), 116),
+('Kho C', 20, 2, NOW(), 117),
+('Kho C', 20, 2, NOW(), 118),
+('Kho C', 20, 2, NOW(), 119),
+('Kho C', 20, 2, NOW(), 120),
+('Kho D', 50, 15, NOW(), 121),
+('Kho D', 50, 15, NOW(), 122),
+('Kho D', 50, 15, NOW(), 123),
+('Kho D', 50, 15, NOW(), 124),
+('Kho D', 50, 15, NOW(), 125),
+('Kho D', 50, 15, NOW(), 126),
+('Kho D', 50, 15, NOW(), 127),
+('Kho D', 50, 15, NOW(), 128),
+('Kho D', 50, 15, NOW(), 129),
+('Kho D', 50, 15, NOW(), 130),
+('Kho D', 50, 15, NOW(), 131),
+('Kho D', 50, 15, NOW(), 132),
+('Kho D', 50, 15, NOW(), 133),
+('Kho D', 50, 15, NOW(), 134),
+('Kho D', 50, 15, NOW(), 135),
+('Kho D', 50, 15, NOW(), 136),
+('Kho D', 50, 15, NOW(), 137),
+('Kho D', 50, 15, NOW(), 138),
+('Kho D', 50, 15, NOW(), 139),
+('Kho D', 50, 15, NOW(), 140),
+('Kho D', 50, 15, NOW(), 141),
+('Kho D', 50, 15, NOW(), 142),
+('Kho D', 50, 15, NOW(), 143),
+('Kho D', 50, 15, NOW(), 144),
+('Kho D', 50, 15, NOW(), 145),
+('Kho D', 50, 15, NOW(), 146),
+('Kho D', 50, 15, NOW(), 147),
+('Kho D', 50, 15, NOW(), 148),
+('Kho D', 50, 15, NOW(), 149),
+('Kho D', 50, 15, NOW(), 150);
+
+INSERT INTO Users (userName, firstname, lastname, passwordUser , role, email) VALUES
+('user1', 'Nguyễn', 'An', '123', 1, 'user1@example.com'),
+('user2', 'Trần', 'Bình', 'password2', 2, 'user2@example.com'),
+('user3', 'Lê', 'Cường', 'password3', 1, 'user3@example.com');
+
+INSERT INTO Discounts (discountDetail, discountCode, discountValue, statusDate, endDate, createAt) VALUES
+('Giảm giá 10% cho đơn hàng trên 1 triệu', 'DISCOUNT10', 10, '2023-10-01', '2023-10-31', NOW()),
+('Giảm giá 50k cho sản phẩm A', 'DISCOUNT50K', 50000, '2023-10-01', '2023-10-15', NOW());
+
+INSERT INTO Orders (orderDate, statusOrder, updateAt, totalAmount, paymentMethod, userId) VALUES
+('2023-10-05', 'confirmationing', NOW(), 2700000, 'transfer', 1),
+('2023-10-06', 'delivering', NOW(), 1500000, 'cash', 2);
+
+INSERT INTO OrderDetails (unitPrice, quatity, statusDetail, productId, orderId) VALUES
+(1500000, 1, 'pending', 1, 1),
+(3000000, 1, 'completed', 2, 1),
+(2500000, 1, 'canceled', 3, 2);
+
+INSERT INTO ProductViews (viewProduct, dayView, productId) VALUES
+(100, '2023-10-05', 1),  -- 100 lượt xem cho sản phẩm có productId = 1 vào ngày 5 tháng 10 năm 2023
+(150, '2023-10-05', 2),  
+(200, '2023-10-06', 1),  
+(50, '2023-10-06', 3);   
+
+SELECT * FROM Orders WHERE orderId = 1
+-- Sport
+INSERT INTO Products (productName, priceBuy, priceSell, productDetail, imageProduct, brandName, createAt, categoryId) VALUES
+('Đồng hồ thông minh Garmin Instinct 2', 4000000, 5390000, 'Đồng hồ thể thao chống nước, nhiều chức năng.', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/_/0/_0002_cf-lg-9f340e51-97bc-4651-b885-00.jpg', 'Garmin', NOW(), 4),
+
+('Đồng hồ thông minh Garmin Instinct 2S', 4000000, 5390000, 'Chăm sóc sức khoẻ tốt hơn - đo nhịp tim, giấc ngủ, tuổi cơ thể...', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/_/0/_0005_cf-lg-65651489-2748-4e13-a2be-87_1_.jpg', 'Garmin', NOW(), 4),
+
+('Đồng hồ thông minh Amazfit T-rex 2', 3000000, 4390000, 'Độ bền chuẩn quân đội - chống chịu nhiệt độ -30 - 70 độ. Kháng nước chuẩn 10 ATM', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/_/0/_0001__-_-___6_2.jpg', 'Amazfit', NOW(), 4),
+
+('Đồng hồ thông minh Garmin Instinct 2X Solar', 8000000, 11390000, 'Sở hữu khả năng sạc bằng năng lượng mặt trời cho phép sử dụng đến 40 ngày ở chế độ thông minh', 'https://cdn2.cellphones.com.vn/358x/media/catalog/product/g/a/garmin-2x-solar-do.jpg','Garmin', NOW(), 4),
+
+('Đồng hồ thông minh Garmin Instinct 3 Solar', 8200000, 11350000, 'Hệ thống GPS chính xác giúp người dùng dễ dàng định vị và ghi lại hành trình.','https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_42__3_33.png','Garmin', NOW(), 4),
+
+('Đồng hồ thông minh Garmin Forerunner 965', 1200000, 15550000, 'Thời lượng pin dài lên đến 30 ngày ở chế độ tiết kiệm pin, 31 giờ khi bật GPS liên tục','https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/g/a/garmin-forerunner-965-trang-1.jpg','Garmin', NOW(), 4),
+
+('Đồng hồ thông minh Samsung Galaxy Watch7 LTE 40mm', 4000000, 6550000, 'Chống nước 5 ATM - ISO 22810:2010 (Tắm, bơi vùng nước nông)','https://cdn.tgdd.vn/Products/Images/7077/327696/samsung-galaxy-watch7-lte-40mm-1-750x500.jpg','Samsung', NOW(), 4),
+
+('Đồng hồ thông minh BeFit Sporty 3 46.4mm', 900000, 1500000, 'Dài 46.4 mm - Ngang 46.4 mm - Dày 11.65 mm - Nặng 45 g','https://cdn.tgdd.vn/Products/Images/7077/328630/befit-sporty-3-rd-1-1-750x500.jpg','BeFit', NOW(), 4),
+
+('Đồng hồ thông minh Garmin Vivoactive 5 42.2mm dây silicone', 4900000, 5700000, 'Màn hình luôn hiển thị - Phát hiện té ngã','https://cdn.tgdd.vn/Products/Images/7077/315897/garmin-vivoactive-5-day-silicone-xanh-1-750x500.jpg','Garmin', NOW(), 4),
+
+('Đồng hồ định vị trẻ em Kidcare K1', 1900000, 2700000, 'Cuộc gọi video 4G/LTE - Kháng nước IPX8 (Rửa tay, đi mưa)','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7077/329076/dong-ho-dinh-vi-tre-em-kidcare-k1-hong-hc-1-638617571963531395-750x500.jpg','Kidcare', NOW(), 4),
+
+('Đồng hồ G-Shock 49.6 mm Nam GST-B400CX-1ADR', 7900000, 9550000, 'Carbon + Thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333711/g-shock-gst-b400cx-1adr-nam-2-638727968309121890-750x500.jpg','G-SHOCK', NOW(), 4),
+
+('Đồng hồ Baby-G BGA-290 41.5 mm Nữ BGA-290DS-2ADR', 1750000, 2359000, 'Thời gian sử dụng pin: khoảng 3 năm','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333713/baby-g-bga-290ds-2adr-nu-1-638727973930649609-750x500.jpg','BABY-G', NOW(), 4),
+
+('Đồng hồ Casio 43.4 mm Nam MTP-E715D-7AVDF', 1550000, 1959000, 'Dây đeo: Thép không gỉ mạ Ion','https://cdn.tgdd.vn/Products/Images/7264/302236/casio-mtp-e715d-7avdf-nam-10.jpg','Casio', NOW(), 4),
+
+('Đồng hồ CASIO 41.5 mm Nam MTP-V300B-1AUDF', 1350000, 1799000, 'Dây đeo: Thép không gỉ mạ Ion','https://cdn.tgdd.vn/Products/Images/7264/268649/casio-mtp-v300b-1audf-nam-2-750x500.jpg','Casio', NOW(), 4),
+
+('Đồng hồ Casio 37.6 mm Nữ LWS-2200H-4AVDF', 735000, 1009000,'Tên bộ máy: 3557','https://cdn.tgdd.vn/Products/Images/7264/307990/casio-lws-2200h-4avdf-nu-13.jpg','Casio', NOW(), 4),
+
+('Đồng hồ Casio 50.5 × 50.4 mm Nam DW-291HX-1AVDF', 830000, 1239000,'Chất liệu mặt kính: kính khoáng Mineral','https://cdn.tgdd.vn/Products/Images/7264/302230/g-shock-dw-291hx-1avdf-nam-11.jpg','Casio', NOW(), 4),
+
+('Đồng hồ G-Shock 2100 45.4 mm Nam GA-B2100CD-1A4DR', 3720000, 4339000,'Chất liệu khung viền: Carbon + Nhựa Resin','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333709/g-shock-ga-b2100cd-1a4dr-nam-1-638727243007742471-750x500.jpg','G-SHOCK', NOW(), 4),
+
+('Đồng hồ G-Shock 40.2 mm Nữ GMA-P2100SG-2ADR', 1728000, 2479000,'Chất liệu khung viền: Carbon + Nhựa Resin','https://cdn.tgdd.vn/Products/Images/7264/326930/g-shock-gma-p2100sg-2adr-nu-1-750x500.jpg','G-SHOCK', NOW(), 4),
+
+('Đồng hồ G-Shock 2100 44.4 mm Nam GM-B2100AD-2ADR', 10028000, 13009000,'Khung viền: thép không gỉ','https://cdn.tgdd.vn/Products/Images/7264/326934/g-shock-gm-b2100ad-2adr-nam-1-750x500.jpg','G-SHOCK', NOW(), 4),
+-- 19
+('Đồng hồ Citizen 40 mm Nam BI5127-51H', 1028000, 3663000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333115/citizen-bi5127-51h-nam-1-638702014664181835-750x500.jpg','Citizen', NOW(), 2),
+
+('Đồng hồ Citizen 31 mm Nữ EL3100-55Z', 1520000, 2751000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/316397/citizen-el3100-55z-nu-1-638702036045102784-750x500.jpg','Citizen', NOW(), 2),
+
+('Đồng hồ Citizen 40 mm Nam BI5120-51L', 1520000, 2751000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333110/citizen-bi5120-51l-nam-1-638702011319934933-750x500.jpg','Citizen', NOW(), 2),
+
+('Đồng hồ Citizen 40 mm Nam BI5124-50L', 2490000, 3341000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/333114/citizen-bi5124-50l-nam-1-638702014050674289-750x500.jpg','Citizen', NOW(), 2),
+
+('Đồng hồ Orient Vietnam Special Edition 41.5 mm Nam RA-AS0105S30B', 8770000, 10991000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/332551/orient-ra-as0105s30b-nam-3-638684895743473550-750x500.jpg','ORIENT', NOW(), 2),
+
+('Đồng hồ Citizen Tsuyosa 40 mm Nam NJ0152-51X', 8020000, 10263000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/330001/citizen-nj0152-51x-nam-9-638629597846393245-750x500.jpg','Citizen', NOW(), 2),
+
+('Đồng hồ Orient Vietnam Special Edition 41.5 mm Nam RA-AS0106L30B', 1780000, 13660000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/332552/orient-ra-as0106l30b-nam-2-638684901290354468-750x500.jpg','ORIENT', NOW(), 2),
+
+('Đồng hồ Orient 41 mm Nam RE-AV0127L00B', 17800000, 23885000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/331127/orient-re-av0127l00b-nam-6-638654688173917403-750x500.jpg','ORIENT', NOW(), 2),
+
+('Đồng hồ Orient Vietnam Special Edition 39.3 mm Nam RE-AT0018S00B', 20899000, 25698000,'Khung viền: thép không gỉ','https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/332550/orient-re-at0018s00b-nam-8-638684895456476801-750x500.jpg','ORIENT', NOW(), 2),
+
+('BAUME & MERCIER CHRONOGRAPH MOONPHASE REF. 6102.099', 27899000, 31001000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/BAUMER_MERCIER6102-099-14.jpg?v=1726332359&width=990','Baume & Mercier', NOW(), 2),
+
+('OMEGA CONSTELLATION CHRONOMETER AUTOMATIC REF. 168.018', 19999000, 23031000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/BAUMER_MERCIER6102-099-14.jpg?v=1726332359&width=990','OMEGA', NOW(), 2),
+
+('UNIVERSAL GENÈVE GILT SHADOW AUTOMATIC REF. 566101', 9799000, 12031000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/UG566101_4_-1.jpg?v=1738569170&width=990','Universal Genève', NOW(), 2),
+
+('UNIVERSAL GENÈVE WHITE SHADOW AUTOMATIC ULTRA-SLIM REF. 866105', 1417000, 18129000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF1430.jpg?v=1732244968&width=990','Universal Genève', NOW(), 2),
+
+('UNIVERSAL GENÈVE GILT SHADOW AUTOMATIC ULTRA-SLIM REF. 566105', 1415000, 18192000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF1522.jpg?v=1732612554&width=990','Universal Genève', NOW(), 2),
+
+('LONGINES FLAGSHIP AUTOMATIC REF. 3401-4', 1235000, 16890000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/LONGINESFLAGSHIP3104-4-1.jpg?v=1741248098&width=990','LONGINES', NOW(), 2),
+
+('LONGINES FLAGSHIP REF. 7686-13', 4350000, 6481000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/LONGINES7686-13-1.jpg?v=1741773520&width=990','LONGINES', NOW(), 2),
+
+('LONGINES REF. 8446-5', 4350000, 6481000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/LONGINES8446-5-1.jpg?v=1739675149&width=990','LONGINES', NOW(), 2),
+
+('LONGINES AUTOMATIC REF. 4246', 5772000, 9343000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/LONGINES4246_1_-16.jpg?v=1726632724&width=990','LONGINES', NOW(), 2),
+
+('LONGINES REF. 4150', 4752000, 6441000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF9266.jpg?v=1727253432&width=990','LONGINES', NOW(), 2),
+
+('LONGINES AUTOMATIC REF. 3617', 6755000, 9083000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF1451.jpg?v=1732196629&width=990','LONGINES', NOW(), 2),
+
+('LONGINES ULTRA-CHRON AUTOMATIC REF. 7851', 8805000, 11185000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF1722.jpg?v=1733277882&width=990','LONGINES', NOW(), 2),
+
+('LONGINES ULTRA-CHRON AUTOMATIC REF. 8442-1', 10000000, 14288000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF4216.jpg?v=1730257003&width=990','LONGINES', NOW(), 2),
+
+-- 22
+
+('LONGINES ULTRA-CHRON AUTOMATIC REF. 8042-1', 7033000, 11946000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/LONGINESULTRA-CHRON8024-1_1_-1.jpg?v=1740112876&width=990','LONGINES', NOW(), 2),
+
+('BAUME & MERCIER HAMPTON AUTOMATIC REF. 65308', 1055700, 14286000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/BAUME_MERCIERHAMPTON65308-14.jpg?v=1735900612&width=990','Baume & Mercier', NOW(), 2),
+
+('OMEGA AUTOMATIC REF. 3971-4-SC', 10999000, 14339000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/OMEGA3971-5-15.jpg?v=1728505690&width=990','OMEGA', NOW(), 2),
+
+('OMEGA DE VILLE AUTOMATIC REF. 161.021', 8989000, 12209000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/OMEGA161021_1_-15.jpg?v=1723603744&width=990','OMEGA', NOW(), 2),
+
+('OMEGA DE VILLE AUTOMATIC REF. 151.010', 7009000, 11949000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/OMEGADEVILLE151010_5_-15.jpg?v=1723603921&width=990','OMEGA', NOW(), 2),
+
+('OMEGA GENÈVE AUTOMATIC REF. 162.0010', 7809000, 10940000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/OMEGADEVILLE151010_5_-15.jpg?v=1723603921&width=990','OMEGA', NOW(), 2),
+
+('OMEGA GENÈVE DYNAMIC AUTOMATIC REF 166.107', 10293000, 15589000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF1916.jpg?v=1734165768&width=990','OMEGA', NOW(), 2),
+
+('OMEGA GENÈVE AUTOMATIC REF. 162.0042', 7993000, 11685000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/P1345558.jpg?v=1720079523&width=990','OMEGA', NOW(), 2),
+
+('OMEGA GENÈVE AUTOMATIC REF. 162.010', 7993000, 10384000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/DSCF4504.jpg?v=1730867496&width=990','OMEGA', NOW(), 2),
+
+('OMEGA SEAMASTER AUTOMATIC REF. 2975-5-SC', 17999000, 22085000,'Khung viền: thép không gỉ','https://www.zeitvintagewatch.com/cdn/shop/files/OMEGASEAMASTER2975-5-SC-1.jpg?v=1741069953&width=990','OMEGA', NOW(), 2);
+-- 10
+
+
